c09cf2f154bd41f96c089b1ba128526d5bda07b7
dhinesh20022806 <dhinesh20022806@gmail.com>
2024-06-09 15:57:25 +0530

    :construction: Work in progress

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,7 @@ app.use(logger('dev'));
 app.use( "/signup",signupRouter);
 app.use('/login', loginRouter);
 
-// app.use(authMiddleWare);
-verifyTokens(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJ1c2VyX2VtYWlsIjoibWVlbmFAZ21haWwuY29tIiwiaWF0IjoxNzE3ODMxNjkyLCJleHAiOjE3MTc4MzUyOTJ9.G2tsBB38vtdgp8utez_EaEkbx18dwqjU6s9U3PsrdbE'
-);
-// app.use(homeRouter)
+app.use(authMiddleWare);
+
+app.use(homeRouter)
 module.exports = app;
